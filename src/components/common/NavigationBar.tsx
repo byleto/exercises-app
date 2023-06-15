fb75faa11b0002786284806fbf1acd981b58aa20
Byron Torres <byleto@gmail.com>
2023-06-15 16:16:04 +0200

    minor ui change

--- src/components/common/NavigationBar.tsx
+++ src/components/common/NavigationBar.tsx
@@ -4,17 +4,17 @@ export const NavigationBar = () => {
   return (
     <Nav variant="pills">
       <Nav.Item>
-        <Nav.Link href="/" eventKey="1" title="Item">
+        <Nav.Link href="/" eventKey="1" title="Home">
           Home
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="/exercises" eventKey="2" title="Item">
+        <Nav.Link href="/exercises" eventKey="2" title="Exercises">
           Exercises
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="/create" eventKey="3" title="Item">
+        <Nav.Link href="/create" eventKey="3" title="Create">
           Create
         </Nav.Link>
       </Nav.Item>
